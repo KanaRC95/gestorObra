36ed2e0b447f042141b8902814eaa906cf1e2ad5
madportable <kname3official@gmail.com>
2022-10-04 15:19:17 -0300

    29 Sep 2022

--- App.py
+++ App.py
@@ -14,7 +14,7 @@ store.initialize()
 
 app = Flask(__name__, template_folder="templates")
 app.config['SECRET_KEY'] = '7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fe'
-
+data = []
 
 class User(UserMixin):
     def __init__(self, id, name, email, password, is_admin=False):
@@ -31,13 +31,27 @@ class User(UserMixin):
         return '<User {}>'.format(self.email)
 
 class Material:
-    def __init__(self, name, cant, unit, type, proveedor, contacto):
+    def __init__(self, name, unit, type, proveedor, contacto):
         self.name = name
         self.unit = unit
         self.contacto = contacto
-        self.cant = cant
         self.type = type
         self.proveedor = proveedor
+
+    def getName(self):
+        return self.name
+
+    def getUnit(self):
+        return self.unit
+
+    def getCon(self):
+        return self.contacto
+
+    def getType(self):
+        return self.type
+
+    def getSource(self):
+        return self.proveedor
 
 def login ():
     data = []
@@ -46,27 +60,52 @@ def login ():
 
 @app.route('/')
 def index ():
-    data = []
-    return render_template('index.html', contacts=data)
+    return render_template('index.html', data=data)
 
-@app.route('/materiales')
+@app.route('/mats')
 def mats ():
+    temp =[]
     data = []
-    return render_template('materiales.html', contacts=data)
-current_user = User("asd110","brian","a@a.com","1221321")
+    mats = queryMats()
+    for x in mats:
+        temp.append(x.getName())
+        temp.append(x.getUnit())
+        temp.append(x.getType())
+        temp.append(x.getSource())
+        temp.append(x.getCon())
+        data.append(temp)
+        temp = []
+    print(data)
+    return render_template('materiales.html', mats=data)
 
 
-def queryObject(self):
+@app.route('/add_mat', methods=['POST'])
+def add_mats():
+    if request.method == 'POST':
+        name = request.form['mname']
+        units = request.form['units']
+        tipo = request.form['type']
+        proveedor = request.form['source']
+        contacto = request.form['pcont']
+
+        material = Material(name,units,tipo,proveedor,contacto)
+        with store.open_session() as session:
+            session.store(material)
+            session.save_changes()
+            store.close()
+        return redirect(url_for('mats'))
+
+def queryMats():
     with store.open_session() as session:
         matList = list(  # Materialize query
             session
             .query(object_type=Material)  # Query for Products
             # .where_greater_than("UnitsInStock", 5)  # Filter
             # .skip(0).take(10)                       # Page
-            .select("name", "year", "rating")  # Project
+            .select("name", "unit", "contacto", "type", "proveedor")  # Project
         )
     store.close()
-    return pelisList
+    return matList
 
 if __name__=="__main__":
         app.run(port=3000,debug=True)
